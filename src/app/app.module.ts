2b844e9dc94a5845d6f3839f014a44ab09adb1ce
Venkatesh Nagireddy <44273845+v3nk3y@users.noreply.github.com>
2023-07-17 20:35:35 +0000

    Revert "Add firebase config"
    This reverts commit d40dcf9f542fdb40fb4d3ce00e18105f9eb0df23.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // Shared
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
-import { environment } from '../environments/environment';
-
-// Firebase
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -24,10 +18,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
